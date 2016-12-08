d22f36e7a9a1b8bd6a789448fd133aee782809b3
kaynatq <kquayyum@dons.usfca.edu>
2016-12-07 19:30:39 -0800

    edit code

--- Project-CS686.R
+++ Project-CS686.R
@@ -17,6 +17,8 @@ x$Step.End.Time <- as.numeric(x$Step.End.Time, na.rm = TRUE)
 train <- (Row > 100000)
 x.test <- x[!train,]
 Correct.First.Attempt.test <- Correct.First.Attempt[!train]
+
+# logistic regression
 glm.correct <- glm(Correct.First.Attempt~ First.Transaction.Time + Step.Start.Time + Step.End.Time +
                      Correct.Transaction.Time + Incorrects, data = x,
                    family = binomial, subset = train)
@@ -36,12 +38,16 @@ plot(Hints, Correct.First.Attempt, main="Correct First Attempt VS Hints",
      xlab="Hints", ylab = "Correct First Attempt")
 mean(pred.glm==Correct.First.Attempt.test)
 
+
+# linear discriminant analysis
 glm.lda <- lda(Correct.First.Attempt~ Step.Start.Time + Step.End.Time +
                  Correct.Transaction.Time + Hints + Corrects + Incorrects,subset = train)
 prediction.lda <- predict(glm.lda, x.test)
 
 table(Correct.First.Attempt.test, prediction.lda$class)
 mean(prediction.lda$class==Correct.First.Attempt.test)
+
+# quadratic discriminant analysis
 fit.qda <- qda(Correct.First.Attempt~ Step.Start.Time + Step.End.Time +
                  Correct.Transaction.Time + Hints + Corrects + Incorrects,subset = train)
 pred.qda <- predict(fit.qda, x.test)
@@ -62,3 +68,30 @@ x1$Step.Start.Time <- as.numeric(x1$Step.Start.Time)
 View(x1)
 x1$Correct.Transaction.Time <- as.numeric(x1$Correct.Transaction.Time)
 x1$Step.End.Time <- as.numeric(x1$Step.End.Time, na.rm = TRUE)
+
+train <- (Row > 100000)
+x1.test <- x1[!train,]
+Correct.First.Attempt.test <- Correct.First.Attempt[!train]
+
+# This computation takes around 30 mins to generate output
+glm.correct <- glm(Correct.First.Attempt~ First.Transaction.Time + Step.Start.Time + Step.End.Time +
+                     Correct.Transaction.Time + Incorrects + dummyKC + 
+                     dummyOpportunity, data = x1,
+                   family = binomial, subset = train)
+
+summary(glm.correct)
+prediction <- predict(glm.correct, data = x1.test, type = "response")
+pred.glm <- rep(0, length(Correct.First.Attempt.test))
+pred.glm[prediction > 0.5] <- 1
+table(pred.glm, Correct.First.Attempt.test)
+
+mean(pred.glm==Correct.First.Attempt.test)
+
+# this one also takes 15-20 mins to run
+glm.lda <- lda(Correct.First.Attempt~ Step.Start.Time + Step.End.Time +
+                 Correct.Transaction.Time + Hints + Corrects + Incorrects +
+               dummyKC + dummyOpportunity, data = x1, subset = train)
+prediction.lda <- predict(glm.lda, x1.test)
+
+table(Correct.First.Attempt.test, prediction.lda$class)
+mean(prediction.lda$class==Correct.First.Attempt.test)
